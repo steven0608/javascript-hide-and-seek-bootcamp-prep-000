deb58c1854a3bccbf1fb319857796b0df03a3a0a
Steven Chen <ysteven.chen2@gmail.com>
2018-04-03 17:34:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector){
   var firstElement = document.querySelectorAll(selector)[0];
   return firstElement;
 }
+
+function nestedTarget(){
+  var element = document.querySelectorAll(#nested.target);
+  return element;
+}
