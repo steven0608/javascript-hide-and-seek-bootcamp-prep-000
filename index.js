dac4a50aef2a9f241602e9386b38547e21f25f1d
Steven Chen <ysteven.chen2@gmail.com>
2018-04-03 18:07:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector){
+  var firstElement = document.querySelectorAll(selector)[0];
+  return firstElement;
+}
+
+function nestedTarget(){
+  var element = document.getElementById("nested").querySelectorAll(".target")[0];
+  return element;
+}
+function increaseRankBy(n){
+  var rank=document.querySelectorAll(".ranked-list")
+  for (let i=0;i<rank.length;i++){
+   rank[i].innerHTML=parseInt(rank[i].innerHTML)+parseInt(n);
+  }
+  return rank;
+}
+
+function deepestChild(){
+  var current=document.querySelectorAll("#grand-node")[0]
+  var next = current.children[0]
+  while (next){
+    current=next;
+    next=current.children[0]
+  }
+  return current;
+}
