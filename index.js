8085842b1416d5ba7fca7efbaffc0de57b7d8d57
Steven Chen <ysteven.chen2@gmail.com>
2018-04-03 17:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function nestedTarget(){
   return element;
 }
 function increaseRankBy(n){
-  var rank=parseInt(document.querySelectorAll(".ranked-list").innerHTML)
-
+  var rank=parseInt(document.querySelectorAll(".ranked-list").)
+  for (let i=0;i<rank.length;i++){
+    const newRank=rank[i].innerHTML+n;
+  }
   return (rank+parseInt(n));
 }
